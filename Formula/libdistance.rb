5f46ddbe8a32dbc1b76dc24ab495ce31313deea7
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-05-25 21:50:37 -0230

    Add libdistance formula.
    This library is used by libgrading to calculate string distances.

---
+++ Formula/libdistance.rb
@@ -0,0 +1,21 @@
+class Libdistance < Formula
+  desc "compute the distance between two pieces of data"
+  homepage "https://monkey.org/~jose/software/libdistance/"
+  url "http://monkey.org/~jose/software/libdistance/libdistance-0.2.2.tar.gz"
+  sha256 "65364e07c9fe78ef34fc0e563aff2317ab0aba7549da07adea929426a708e6ad"
+
+  def install
+    system "make"
+    system "make", "-C", "test"
+
+    include.install "distance.h"
+    lib.install "libdistance.a"
+
+    mkdir "#{prefix}/test"
+    cp "test/test", "#{prefix}/test/"
+  end
+
+  test do
+    system "#{prefix}/test/test"
+  end
+end
