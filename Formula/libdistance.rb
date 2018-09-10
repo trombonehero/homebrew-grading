4d5579864be9a62b3ddc201e9ea6541bf9b4f658
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-09-10 15:46:31 -0230

    Update libdistance to v0.2.4-beta2.

--- Formula/libdistance.rb
+++ Formula/libdistance.rb
@@ -1,8 +1,8 @@
 class Libdistance < Formula
   desc "compute the distance between two pieces of data"
   homepage "https://monkey.org/~jose/software/libdistance/"
-  url "https://github.com/trombonehero/libdistance/archive/v0.2.3.tar.gz"
-  sha256 "5f488394adcc72a490097ee6b87e4e0f5dbedd2e8d54902a4a4a8b970889756c"
+  url "https://github.com/trombonehero/libdistance/archive/v0.2.4-beta2.tar.gz"
+  sha256 "4331ec86a88054fe6cbeba9639a84bfc6f144c2626a0954d2ef776b03c73daed"
 
   def install
     system "make"
