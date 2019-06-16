42ff91ab7d7983f11371a236344aa434a458ca9c
Jonathan Anderson <jonathan.anderson@ieee.org>
2019-06-16 16:25:50 -0230

    Update to v0.1.8

--- Formula/libgrading.rb
+++ Formula/libgrading.rb
@@ -1,8 +1,8 @@
 class Libgrading < Formula
   desc "A library for grading C- and C++-language assignments"
   homepage "https://trombonehero.github.io/libgrading"
-  url "https://github.com/trombonehero/libgrading/archive/v0.1.7.tar.gz"
-  sha256 "8d7d35a2bdb7ad74be2be3886f1d9616e938ba52045d22c0534bd7d620640040"
+  url "https://github.com/trombonehero/libgrading/archive/v0.1.8.tar.gz"
+  sha256 "2a65c2ec9ed35d8f2035844c71004dbf42eec9827204e9a31b9addbcd0456763"
 
   depends_on "cmake" => :build
   depends_on "libdistance" => :build
